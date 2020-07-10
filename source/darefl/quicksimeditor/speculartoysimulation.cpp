f1d15f6dc37f1d2920c7514e6901d6a5fe1e8428
Randolf Beerwerth <r.beerwerth@fz-juelich.de>
2020-07-10 14:17:36 +0200

    Small simulation kernel for scalar specular computation

--- source/darefl/quicksimeditor/speculartoysimulation.cpp
+++ source/darefl/quicksimeditor/speculartoysimulation.cpp
@@ -10,14 +10,20 @@
 #include <darefl/quicksimeditor/fouriertransform.h>
 #include <darefl/quicksimeditor/materialprofile.h>
 #include <darefl/quicksimeditor/speculartoysimulation.h>
+#include <darefl/minikernel/Material/MaterialFactoryFuncs.h>
+#include <darefl/minikernel/Computation/Slice.h>
+#include <darefl/minikernel/MultiLayer/LayerRoughness.h>
+#include <mvvm/standarditems/axisitems.h>
 #include <mvvm/utils/containerutils.h>
 #include <stdexcept>
 #include <thread>
+#include <algorithm>
 
 namespace
 {
-const int delay_mksec = 500;
-const int simulation_steps_count = 500;
+//const int delay_mksec = 500;
+const int simulation_points = 500;
+std::vector<BornAgain::Slice> createBornAgainSlices(const multislice_t& multislice);
 } // namespace
 
 using namespace ModelView;
@@ -29,48 +35,31 @@ SpecularToySimulation::SpecularToySimulation(const multislice_t& multislice)
 
 void SpecularToySimulation::runSimulation()
 {
+    auto slices = createBornAgainSlices(input_data);
+    auto qvalues = ModelView::FixedBinAxisItem::create(simulation_points, specular_result.xmin, specular_result.xmax)->binCenters();
 
-    /* wish for actual procedure
-    // auto std::vector<BA::Slices> processedSlices(input_data);
+    specular_result.data.reserve(simulation_points);
 
     progress_handler.reset();
-    for (int i = 0; i < simulation_steps_count; ++i) {
-
+    for( auto q : qvalues)    {
         if (progress_handler.has_interrupt_request())
             throw std::runtime_error("Interrupt request");
 
-        auto q = …;
-        k = 0.5 * q;
-
-        auto result = strategy.Execute(processedSlices, kvector_t(0, 0, k)).front();
-        specular_result[i] = SpecularIntensity(result.front());
-        progress_handler.setCompletedTicks(1);
-    }
-    */
-
-    // actual simulation
-    auto [xmin, xmax] = MaterialProfile::DefaultMaterialProfileLimits(input_data);
-    auto profile =
-        MaterialProfile::CalculateProfile(input_data, simulation_steps_count, xmin, xmax);
-    auto specular = fourier_transform(profile);
-    specular_result.xmin = 0.0;
-    specular_result.xmax = specular.size();
-    specular_result.data = ModelView::Utils::Real(specular);
-
-    // Mimicking long simulation time, reporting progress and canceling, if necessary.
-    progress_handler.reset();
-    for (int i = 0; i < simulation_steps_count; ++i) {
-        if (progress_handler.has_interrupt_request())
-            throw std::runtime_error("Interrupt request");
+        auto kz = 0.5 * q;
+        auto coeff = std::move( strategy->Execute(slices, kvector_t(0, 0, -kz)).front() );
+        specular_result.data.emplace_back( std::norm( coeff->getScalarR() ) );
 
         progress_handler.setCompletedTicks(1);
-        std::this_thread::sleep_for(std::chrono::microseconds(delay_mksec));
     }
+
+    // temporarily log it by hand
+    std::for_each(specular_result.data.begin(), specular_result.data.end(), [](auto & value){value = std::log(value);});
+
 }
 
 void SpecularToySimulation::setProgressCallback(ModelView::ProgressHandler::callback_t callback)
 {
-    progress_handler.setMaxTicksCount(simulation_steps_count);
+    progress_handler.setMaxTicksCount(simulation_points);
     progress_handler.subscribe(callback);
 }
 
@@ -86,3 +75,23 @@ SpecularToySimulation::Result SpecularToySimulation::sld_profile(const multislic
     auto profile = MaterialProfile::CalculateProfile(multislice, n_points, xmin, xmax);
     return {xmin, xmax, ModelView::Utils::Real(profile)};
 }
+
+namespace
+{
+    std::vector<BornAgain::Slice> createBornAgainSlices(const multislice_t& multislice)
+    {
+        std::vector<BornAgain::Slice> result;
+        result.reserve(multislice.size());
+
+        for(auto & slice : multislice)
+        {
+            auto material = MaterialBySLD("", slice.material.real(), slice.material.imag());
+            auto roughness = LayerRoughness(slice.sigma, 0., 0.);
+
+            result.emplace_back(slice.thickness, material, roughness);
+        }
+
+        return result;
+    }
+}
+
